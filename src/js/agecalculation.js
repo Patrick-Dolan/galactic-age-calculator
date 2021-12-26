5fff3d1c4480176c7863e597ec62f2c5b67a9cfe
Patrick Dolan <dolanp1992@gmail.com>
2021-12-26 14:12:14 -0800

    add jupiter to constructor and calculatePlanetAges method

--- src/js/agecalculation.js
+++ src/js/agecalculation.js
@@ -10,10 +10,14 @@ export class AgeCalculation {
     this.mars = {
       solarAge: 0
     }
+    this.jupiter = {
+      solarAge: 0
+    }
   }
   calculatePlanetAges(){
     this.mercury.solarAge = parseFloat((this.age / 0.24).toFixed(2));
     this.venus.solarAge = parseFloat((this.age / 0.62).toFixed(2));
     this.mars.solarAge = parseFloat((this.age / 1.88).toFixed(2));
+    this.jupiter.solarAge = parseFloat((this.age / 11.86).toFixed(2));
   }
 }
