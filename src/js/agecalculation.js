91e4537cc844995f5651ad659f0bc3292c5276e1
Patrick Dolan <dolanp1992@gmail.com>
2021-12-26 13:54:15 -0800

    add calculatePlanetAges method

--- src/js/agecalculation.js
+++ src/js/agecalculation.js
@@ -1,5 +1,11 @@
 export class AgeCalculation {
   constructor(age) {
     this.age = age;
+    this.mercury = {
+      solarAge: 0
+    }
+  }
+  calculatePlanetAges(){
+    this.mercury.solarAge = parseInt((this.age / 0.24).toFixed(2));
   }
 }
