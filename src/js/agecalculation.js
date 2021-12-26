8e60a56a6264e3e7d82c1416dce4ec77d507abd1
Patrick Dolan <dolanp1992@gmail.com>
2021-12-26 14:03:29 -0800

    add venus to calculatePlanetAges and generated debugging json

--- src/js/agecalculation.js
+++ src/js/agecalculation.js
@@ -4,8 +4,12 @@ export class AgeCalculation {
     this.mercury = {
       solarAge: 0
     }
+    this.venus = {
+      solarAge: 0
+    }
   }
   calculatePlanetAges(){
-    this.mercury.solarAge = parseInt((this.age / 0.24).toFixed(2));
+    this.mercury.solarAge = parseFloat((this.age / 0.24).toFixed(2));
+    this.venus.solarAge = parseFloat((this.age / 0.62).toFixed(2));
   }
 }
