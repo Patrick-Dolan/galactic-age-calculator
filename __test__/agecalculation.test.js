71227064884da73b2916523db8fea68d1da24338
Patrick Dolan <dolanp1992@gmail.com>
2021-12-26 13:41:09 -0800

    add test for calculatePlanetAges method and beforeAll setup

--- __test__/agecalculation.test.js
+++ __test__/agecalculation.test.js
@@ -1,9 +1,22 @@
 import { AgeCalculation } from "./../src/js/agecalculation";
 
+let age;
+let ageCalculation;
+beforeAll(() => {
+  age = 30;
+  ageCalculation = new AgeCalculation(age);
+});
 describe("AgeCalculation", () => {
   test("should declare an ageCalculation object containing an age", () => {
     let age = 30;
     let ageCalculation = new AgeCalculation(age);
     expect(ageCalculation.age).toEqual(30);
   });
+  // calculatePlanetAges method tests
+  describe("calculatePlanetAges", () => {
+    test("should correctly calculate solar age on mercury", () => {
+      ageCalculation.calculatePlanetAges();
+      expect(ageCalculation.mercury.age).toEqual(125.00);
+    });
+  })
 });
