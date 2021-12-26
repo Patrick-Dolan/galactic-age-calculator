3377356bc0cb55b5d52e4b0746df883a09aaed66
Patrick Dolan <dolanp1992@gmail.com>
2021-12-26 14:08:24 -0800

    add mars to calculatePlanetAges method

--- src/js/agecalculation.js
+++ src/js/agecalculation.js
@@ -7,9 +7,13 @@ export class AgeCalculation {
     this.venus = {
       solarAge: 0
     }
+    this.mars = {
+      solarAge: 0
+    }
   }
   calculatePlanetAges(){
     this.mercury.solarAge = parseFloat((this.age / 0.24).toFixed(2));
     this.venus.solarAge = parseFloat((this.age / 0.62).toFixed(2));
+    this.mars.solarAge = parseFloat((this.age / 1.88).toFixed(2));
   }
 }
