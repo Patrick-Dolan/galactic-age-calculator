9bf82abe1bec736f3079ef548674eeba25a6ebee
Patrick Dolan <dolanp1992@gmail.com>
2021-12-27 12:30:55 -0800

    add ageCalcForm submit button functionality to do calculation

--- src/index.js
+++ src/index.js
@@ -2,3 +2,15 @@ import $ from "jquery";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { AgeCalculation } from "./js/agecalculation";
+
+$("form#ageCalcForm").submit((event) => {
+  event.preventDefault();
+  const age = parseInt($("input#userAge").val());
+  let ageCalc = new AgeCalculation(age);
+  ageCalc.calculatePlanetAges();
+  ageCalc.averageLifeSpans();
+  ageCalc.calculateLifeExpectancy();
+  ageCalc.hasSurpassedLifeExpectancy();
+  console.log(ageCalc);
+});
