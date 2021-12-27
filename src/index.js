35032cf68ef248d9f85aa7436db7417e7b226381
Patrick Dolan <dolanp1992@gmail.com>
2021-12-27 14:14:49 -0800

    add conditional for earth li to show info based on earthAverageLifeExpectancy

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import { AgeCalculation } from "./js/agecalculation";
 function displayPlanetDetails(ageCalc) {
   let planetList = $("ul#planets");
   let htmlForPlanetList = "";
+  if (ageCalc.age > ageCalc.earthAverageLifeSpan) {
+    htmlForPlanetList += `<li>Your age on Earth is ${ageCalc.age} where the average life expectancy is ${ageCalc.earthAverageLifeSpan}. That means that you've lived ${ageCalc.surpassedLifeExpectancy} more years than Earths average life expectancy.</li>`;
+  } else {
+    htmlForPlanetList += `<li>Your age on Earth is ${ageCalc.age} where the average life expectancy is ${ageCalc.earthAverageLifeSpan}. That means that you should live at least ${ageCalc.surpassedLifeExpectancy} more years in Earths solar years.</li>`;
+  }
   Object.keys(ageCalc.planets).forEach((planet) => {
     if (ageCalc.isPastLifeExpectancy) {
       htmlForPlanetList += `<li>Your age on ${planet} would be ${ageCalc.planets[planet].solarAge} where the average life expectancy is ${ageCalc.planets[planet].averageLifeSpan}. That means that you've lived ${ageCalc.planets[planet].surpassedLifeExpectancy} more years than ${planet}'s average life expectancy.</li>`;
