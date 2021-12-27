15a4a31c0e2360f8ea626ab9c61c23108614feb5
Patrick Dolan <dolanp1992@gmail.com>
2021-12-27 12:55:00 -0800

    add displayPlanetDetails function to show calculation results

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { AgeCalculation } from "./js/agecalculation";
 
+function displayPlanetDetails(ageCalc) {
+  let planetList = $("ul#planets");
+  let htmlForPlanetList = "";
+  Object.keys(ageCalc.planets).forEach((planet) => {
+    htmlForPlanetList += `<li>Your age on ${planet} would be ${ageCalc.planets[planet].solarAge} where the average life expectancy is ${ageCalc.planets[planet].lifeExpectancy}.`;
+  });
+  planetList.html(htmlForPlanetList);
+}
+
 $("form#ageCalcForm").submit((event) => {
   event.preventDefault();
   const age = parseInt($("input#userAge").val());
@@ -12,5 +21,6 @@ $("form#ageCalcForm").submit((event) => {
   ageCalc.averageLifeSpans();
   ageCalc.calculateLifeExpectancy();
   ageCalc.hasSurpassedLifeExpectancy();
+  displayPlanetDetails(ageCalc);
   console.log(ageCalc);
 });
