0662a5ce8a1bb16a4107b38267a7ff3ebffdb0f9
Patrick Dolan <dolanp1992@gmail.com>
2021-12-27 14:19:09 -0800

    make it so displayPlanetDetails will capitalize first letter of planet names for display

--- src/index.js
+++ src/index.js
@@ -13,10 +13,11 @@ function displayPlanetDetails(ageCalc) {
     htmlForPlanetList += `<li>Your age on Earth is ${ageCalc.age} where the average life expectancy is ${ageCalc.earthAverageLifeSpan}. That means that you should live at least ${ageCalc.surpassedLifeExpectancy} more years in Earths solar years.</li>`;
   }
   Object.keys(ageCalc.planets).forEach((planet) => {
+    let planetName = planet.charAt(0).toUpperCase() + planet.slice(1);
     if (ageCalc.isPastLifeExpectancy) {
-      htmlForPlanetList += `<li>Your age on ${planet} would be ${ageCalc.planets[planet].solarAge} where the average life expectancy is ${ageCalc.planets[planet].averageLifeSpan}. That means that you've lived ${ageCalc.planets[planet].surpassedLifeExpectancy} more years than ${planet}'s average life expectancy.</li>`;
+      htmlForPlanetList += `<li>Your age on ${planetName} would be ${ageCalc.planets[planet].solarAge} where the average life expectancy is ${ageCalc.planets[planet].averageLifeSpan}. That means that you've lived ${ageCalc.planets[planet].surpassedLifeExpectancy} more years than ${planetName}'s average life expectancy.</li>`;
     } else {
-      htmlForPlanetList += `<li>Your age on ${planet} would be ${ageCalc.planets[planet].solarAge} where the average life expectancy is ${ageCalc.planets[planet].averageLifeSpan}. That means that you shoud live at least ${ageCalc.planets[planet].lifeExpectancy} more years in ${planet} solar years.</li>`;
+      htmlForPlanetList += `<li>Your age on ${planetName} would be ${ageCalc.planets[planet].solarAge} where the average life expectancy is ${ageCalc.planets[planet].averageLifeSpan}. That means that you shoud live at least ${ageCalc.planets[planet].lifeExpectancy} more years in ${planetName} solar years.</li>`;
     }
   });
   planetList.html(htmlForPlanetList);
