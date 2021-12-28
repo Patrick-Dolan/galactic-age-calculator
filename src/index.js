5ddb59a36e8fe11819f2c9be832e043d89b5296b
Patrick Dolan <dolanp1992@gmail.com>
2021-12-27 19:20:42 -0800

    change wording in generated html for planets in displayPlanetDetails function

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ function displayPlanetDetails(ageCalc) {
   let planetList = [$("ul#mercury-planet"), $("ul#venus-planet"), $("ul#mars-planet"), $("ul#jupiter-planet")];
   let htmlForPlanet = "";
   if (ageCalc.age > ageCalc.earthAverageLifeSpan) {
-    htmlForPlanet += `<li>Your age on Earth is ${ageCalc.age} where the average life expectancy is ${ageCalc.earthAverageLifeSpan}.</li><li>That means that you've lived ${ageCalc.surpassedLifeExpectancy} more years than Earths average life expectancy.</li>`;
+    htmlForPlanet += `<li>Your age on Earth is ${ageCalc.age} years old where the average life expectancy is ${ageCalc.earthAverageLifeSpan}.</li><li>That means that you've lived ${ageCalc.surpassedLifeExpectancy} more years than Earths average life expectancy.</li>`;
   } else {
-    htmlForPlanet += `<li>Your age on Earth is ${ageCalc.age} where the average life expectancy is ${ageCalc.earthAverageLifeSpan}.</li><li>That means that you should live at least ${ageCalc.surpassedLifeExpectancy} more years in Earths solar years.</li>`;
+    htmlForPlanet += `<li>Your age on Earth is ${ageCalc.age} years old where the average life expectancy is ${ageCalc.earthAverageLifeSpan}.</li><li>That means that you should live at least ${ageCalc.surpassedLifeExpectancy} more years in Earths solar years.</li>`;
   }
   $("ul#earth-planet").html(htmlForPlanet);
   let increment = 0;
@@ -18,9 +18,9 @@ function displayPlanetDetails(ageCalc) {
     htmlForPlanet = "";
     let planetName = planet.charAt(0).toUpperCase() + planet.slice(1);
     if (ageCalc.isPastLifeExpectancy) {
-      htmlForPlanet += `<li>Your age on ${planetName} would be ${ageCalc.planets[planet].solarAge} where the average life expectancy is ${ageCalc.planets[planet].averageLifeSpan}.</li><li>That means that you've lived ${ageCalc.planets[planet].surpassedLifeExpectancy} more years than ${planetName}'s average life expectancy.</li>`;
+      htmlForPlanet += `<li>Your age on ${planetName} is ${ageCalc.planets[planet].solarAge} years old  where the average life expectancy is ${ageCalc.planets[planet].averageLifeSpan}.</li><li>That means that you've lived ${ageCalc.planets[planet].surpassedLifeExpectancy} more years than ${planetName}'s average life expectancy.</li>`;
     } else {
-      htmlForPlanet += `<li>Your age on ${planetName} would be ${ageCalc.planets[planet].solarAge} where the average life expectancy is ${ageCalc.planets[planet].averageLifeSpan}.</li><li>That means that you shoud live at least ${ageCalc.planets[planet].lifeExpectancy} more years in ${planetName} solar years.</li>`;
+      htmlForPlanet += `<li>Your age on ${planetName} is ${ageCalc.planets[planet].solarAge} years old  where the average life expectancy is ${ageCalc.planets[planet].averageLifeSpan}.</li><li>That means that you shoud live at least ${ageCalc.planets[planet].lifeExpectancy} more years in ${planetName} solar years.</li>`;
     }
     planetList[increment].html(htmlForPlanet);
     increment++;
